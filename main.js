df7848975f730cc9fd71da4ad60d3424baba0b69
Manuel <mnrs94@gmail.com>
2017-07-07 12:13:22 +0200

    Update main.js

--- main.js
+++ main.js
@@ -9,8 +9,8 @@ requirejs.config({
      },
      paths: {
         /*'Phaser': '../../bower_components/phaser/build/phaser'*/
-        /*'Phaser': 'lib/phaser.min'*/ /*El usado originalmente. Version 2.4.4*/
-          'Phaser': 'lib/phaser.min.2-8-1'
+          'Phaser': 'lib/phaser.min' /*El usado originalmente. Version 2.4.4*/
+        /*'Phaser': 'lib/phaser.min.2-8-1'*/ /*Version nueva que lo hace ir peor??*/
      }
 });
 require(['loader'],
